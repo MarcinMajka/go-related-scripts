9df28ff37350181d0956bc9878e9a095ddafa26f
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-09-13 13:32:44 +0200

    Working solution cleanup for a specific sgf format

--- tsumego_solution_cleaner.py
+++ tsumego_solution_cleaner.py
@@ -1,24 +1,38 @@
 import argparse
 import os
 
-def print_sgf(sgf_path):
+def process_sgf(sgf_path):
     with open(sgf_path, 'r') as file:
         sgf = file.read()
-        print(os.path.basename(sgf_path))
-        print(sgf)
-        print("")
-        
+    
+    parentheses_count = 0
+    semicolon_count = 0
+    for i, char in enumerate(sgf):
+        if char == '(':
+            parentheses_count += 1
+            if parentheses_count == 3:
+                sgf = sgf[:i] + ')'
+                break
+        elif char == ';':
+            semicolon_count += 1
+            if semicolon_count == 3:
+                sgf = sgf[:i] + ')'
+                break
+    
+    with open(sgf_path, 'w') as file:
+        file.write(sgf)
+    
+    print(f"Processed: {os.path.basename(sgf_path)}")
+    print(sgf)
+    print("")
 
 def main(root_dir):
-    files = os.listdir(root_dir)
-    files = [os.path.join(root_dir, f) for f in files if f.endswith('.sgf')]
+    files = [os.path.join(root_dir, f) for f in os.listdir(root_dir) if f.endswith('.sgf')]
     for f in files:
-        print_sgf(f)
-    return
-    
+        process_sgf(f)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('parser')
-    parser.add_argument('--root_dir')
+    parser = argparse.ArgumentParser('SGF File Processor')
+    parser.add_argument('--root_dir', required=True, help='Directory containing SGF files')
     args = parser.parse_args()
     main(args.root_dir)
